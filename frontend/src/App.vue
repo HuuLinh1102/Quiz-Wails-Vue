200054cc7db36b4f00de1db0988439f038a340d0
DLcMinhNhat12 <tdmnhatt@gmail.com>
2024-03-07 08:23:40 +0700

    nhatv1

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,10 +1,13 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'</script>
+//import HelloWorld from './components/HelloWorld.vue'
+ import HomeView from './views/HomeView.vue'
+</script>
 
 <template>
   
   <HelloWorld/>
 
+  <HomeView/>
 </template>
 
 <style>
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -2,4 +2,10 @@ import {createApp} from 'vue'
 import App from './App.vue'
 import './style.css';
 
-createApp(App).mount('#app')
+// VueSweetalert2
+import VueSweetalert2 from "vue-sweetalert2";
+import "sweetalert2/dist/sweetalert2.min.css";
+
+const app = createApp(App)
+app.use(VueSweetalert2);
+app.mount('#app')
